c24d914c03216192c81141baa4456a639b63c6c7
TiboV <Tibo_vermeire@icloud.com>
2024-07-28 22:48:22 +0200

    feat: basic navigation with 4 screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailScreen';
+import CartScreen from './screens/CartScreen';
+import OlympicsScreen from './screens/OlympicsScreen';
 
-export default function Page() {
+const Stack = createStackNavigator();
+
+export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Sports</Text>
-        <Text style={styles.subtitle}>This is the first page of your app.</Text>
-      </View>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="Cart" component={CartScreen} />
+      <Stack.Screen name="Olympics" component={OlympicsScreen} />
+    </Stack.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
-});
---
+++ app/screens/OlympicsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const OlympicsScreen = () => {
+    return (
+        <View>
+        <Text>Olympics Screen</Text>
+        </View>
+    );
+}
+
+export default OlympicsScreen;
